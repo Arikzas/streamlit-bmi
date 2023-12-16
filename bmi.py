1e3afd4fe65edae62f8f2b990da9f97ae0b11b21
Arik <71495628+Arikzas@users.noreply.github.com>
2023-12-16 10:46:52 +0700

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import math
+
+
+#Tampilkan judul
+st.title("Penghitung BMI")
+
+#Minta pengguna untuk memasukkan tinggi dan berat badan
+height = st.number_input("Tinggi (meter)", min_value=0.01, placeholder="Masukkan tinggi badan kamu")
+weight = st.number_input("Berat badan (kilogram)", min_value=0.01, placeholder="Masukkan berat badan kamu")
+
+#Hitung BMI
+bmi = weight / (height * height)
+
+#Menampilkan hasil perhitung
+st.write("BMI: ", bmi)
+ #Tampilkan interpretasi BMI
+if bmi < 18.5:
+    st.write("Kurus")
+elif bmi < 25:
+    st.write("Normal")
+elif bmi < 30:
+    st.write("Overweight")
+else:
+    st.write("Obesitas")
+
+
+# def calculate_bmi(height, weight):
+    # """
+    # Menghitung BMI dari tinggi dan berat badan.
+# 
+    # Args:
+        # height: Tinggi badan dalam meter.
+        # wight: Berat badan dalam kilogram.
+    # 
+    # Returns:
+        # Nilai BMI.
+    # """
+# 
+    # return weight / (height * height)
+
+
+# def main():
+    # """
+    # Program penghitung BMI.
+    # """
+
+    # Tampilkan judul
+    # st.title("Penghitung BMI")
+
+    # Minta pengguna untuk memasukkan tinggi dan berat badan
+    # height = st.number_input("Tinggi(meter): ")
+    # weight = st.number_input("Berat badan(kilogram): ")
+
+    # try:
+        # bmi = calculate_bmi(height, weight)
+    # except ZeroDivisionError:
+        # st.error("Berat dan tinggi badan tidak boleh 0")
+
+    # Hitung BMI
+    # bmi = weight / (height * height)
+
+    # Menampilkan hasil perhitungan
+    # st.write("BMI: ", bmi)
+
+    # Tampilkan interpretasi BMI
+    # if bmi < 18.5:
+        # st.write("Kurus")
+    # elif bmi < 25:
+        # st.write("Normal")
+    # elif bmi < 30:
+        # st.write("Overweight")
+    # else:
+        # st.write("Obesitas")
+
+# if __name__ == "__main__":
+    # main()
+
